85187d553e19c617fb600bfb6d406e15972e4000
itphira <it.soporte@phira.es>
2024-05-27 15:07:52 +0200

    Initial commit

---
+++ Controllers/NotificationsController.cs
@@ -0,0 +1,32 @@
+﻿using FirebaseAdmin.Messaging;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class NotificationsController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> SendNotification([FromBody] NotificationRequest request)
+    {
+        var message = new Message()
+        {
+            Token = request.Token,
+            Notification = new Notification()
+            {
+                Title = request.Title,
+                Body = request.Body
+            }
+        };
+
+        var result = await FirebaseMessaging.DefaultInstance.SendAsync(message);
+        return Ok(result);
+    }
+}
+
+public class NotificationRequest
+{
+    public string Token { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+}
---
+++ Startup.cs
@@ -0,0 +1,36 @@
+﻿using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        FirebaseApp.Create(new AppOptions()
+        {
+            Credential = GoogleCredential.FromFile("path/to/serviceAccountKey.json")
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
